46d6e134dfa24cf81ebe58c2ba5610520c589fcc
Malhar Vora <mlvora.2010@gmail.com>
2017-01-30 22:58:23 +0530

    Add About link in navigation bar

--- myschool/app.py
+++ myschool/app.py
@@ -5,20 +5,23 @@ app = Flask(__name__)
 
 class MenuList(list):
     def __init__(self):
-        self.extend(["Home", "About"])
+        self.insert(0, ("Home", "/"))
+        self.insert(1, ("About", "about"))
 
 @app.route("/")
 @app.route("/index")
 def index():
     """Entry point"""
     menus = MenuList()
+    print str(menus)
     return render_template("index.html", menus=menus)
 
 @app.route("/about")
 def about():
     """About us page"""
     menus = MenuList()
-    return render_template("about.html", menus=menus)
+    print str(menus)
+    return render_template("aboutus.html", menus=menus)
     
 
 if __name__ == "__main__":
